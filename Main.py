21af2c64b231bfe49a25cc126d1e67339b146f26
Ryan-Ficklin <ryanpficklin@gmail.com>
2020-02-26 12:51:01 -0600

    moved earth to planetarium

--- Main.py
+++ Main.py
@@ -1,4 +1,3 @@
 from Planet import Planet
 
-earth = Planet("Earth", 7917.5, 92009000, 0)
 print(str(earth))
--- Planetarium.py
+++ Planetarium.py
@@ -3,4 +3,5 @@ from Planet import Planet
 class Planetarium(object):
     def __init__(self):
         self.create()
-    
+        earth = Planet("Earth", 7917.5, 92009000, 0)
+        
