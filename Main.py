617b232e7f223d05219589b5d251764c316aff0f
Ryan-Ficklin <ryanpficklin@gmail.com>
2020-02-28 08:06:49 -0600

    Put planets in array, added menu in main

--- Main.py
+++ Main.py
@@ -1,5 +1,16 @@
 from Planetarium import Planetarium
-
 planetarium1 = Planetarium()
-#oneOrAll = str(input("Would you like to print one planet or all of them? "))
-print(str(planetarium1))
+
+def menu():
+    oneOrAll = str(input("Would you like to print one planet or all of them?(1 or All) "))
+    if(oneOrAll == '1'):
+        planetarium1.printOne()
+    elif(oneOrAll == 'All'):
+        print(str(planetarium1))
+        
+choice = 'Y'
+while(choice == 'Y' or choice == 'y'):
+    menu()
+    choice = str(input("Another?(Y or N) "))
+
+
